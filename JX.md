9ae7856dbb1100fe2a0a5695df6dcf315b44551b
暴炜楠 <452287388@qq.com>
2019-04-24 18:51:07 +0800

    1

---
+++ JX.md
@@ -0,0 +1,58 @@
+##1
+
+```javascript
+let obj = { '1': 1 };
+let oldArr = ['1', 1, obj, true];
+oldArr.forEach(item => {
+  item = 1;
+});
+console.log(oldArr);
+```
+
+<span style="color:green">答案:</span> {%s%}[ '1', 1, { '1': 1 }, true ]{%ends%}
+
+##2 cookie 和 token 都存放在 header 中，为什么不会劫持 token？
+
+##3 浏览器向服务器发送请求，相应数据包被拦截怎么办?
+
+##4 sort 和 reverse 会改变原数组吗?
+
+##5 position:absolute 相对什么
+
+##6 Promise 链式调用的时候怎么终止它?
+
+##7 ts 的 extends、implements 区别
+
+##8 不使用 promise 实现一个 sleep 函数
+
+##9 什么是适配器设计模式？
+
+##10 实现一段代码,判断数组里的值是否都是一样 [1,2,3,4,5] => false [1,1,1,1,1]=>true
+
+##11 实现一个 promise 的 finally 的 polyfill
+
+##12 css 盒模型
+
+##13 事件委托是什么
+
+##14 请解释 undefined 和 not defined 的区别
+
+##15 Electron 如何实现跨进程通信?可以同步吗?
+
+##16 jquery.extend 是深拷贝吗?
+
+##17 如何插入 1000 个 div
+
+##18 git pull 执行了什么
+
+##19 react 有哪些常见的性能优化方法
+
+##20 vue 为什么不能实现对数组的监听
+
+#nodejs
+
+#babel
+
+#webpack
+
+#Electron
---
+++ index.js
@@ -0,0 +1,48 @@
+let obj = { 1: 1 };
+let oldArr = ['1', 1, obj, true];
+oldArr.forEach(item => {
+  item = 1;
+});
+console.log(oldArr);
+// 首先token不是防止XSS的，而是为了防止CSRF的； 2、CSRF攻击的原因是浏览器会自动带上cookie，而浏览器不会自动带上token
+/**
+ * @answer 2
+ */
+
+class B extends A {}
+
+class C implements A {}
+
+console.log(new B() instanceof A); // true
+console.log(new C() instanceof A); // false
+// extends 意味着 - 它从父母那里得到所有
+// implements 在这种情况下几乎就像实现一个接口。子对象可以假装它是父...但它没有得到任何实现
+
+//ES5
+function sleep(callback, time) {
+  if (typeof callback === 'function') setTimeout(callback, time);
+}
+//Generator
+function* sleepGenerator(time) {
+  yield new Promise(function(resolve, reject) {
+    setTimeout(resolve, time);
+  });
+}
+sleepGenerator(1000)
+  .next()
+  .value.then(() => {
+    console.log(1);
+  });
+
+Promise.prototype.finally = function(callback) {
+  return this.then(
+    value => this.constructor.resolve(callback()).then(() => value),
+    reason =>
+      this.constructor.resolve(callback()).then(() => {
+        throw reason;
+      })
+  );
+};
+
+var fragment = document.createDocumentFragment();
+fragment.appendChild(elem);
